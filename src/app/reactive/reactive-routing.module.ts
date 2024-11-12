3d759255a117ffaa399af032e6e987fac0b99de9
kenier <kenier202@gmail.com>
2024-11-12 15:22:56 -0500

    feat(Reactive): add reactive children routes

--- src/app/reactive/reactive-routing.module.ts
+++ src/app/reactive/reactive-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {path: 'basic', component: BasicPageComponent},
+      {path: 'dynamic', component: BasicPageComponent},
+      {path: 'switches', component: BasicPageComponent},
+      {path: '**', component: BasicPageComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
