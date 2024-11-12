e57fca325b4d922044f5a168ca2d61f83bd87e5c
kenier <kenier202@gmail.com>
2024-11-12 16:10:42 -0500

    refactor(Routes): change the rendering components

--- src/app/reactive/reactive-routing.module.ts
+++ src/app/reactive/reactive-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
       {path: 'basic', component: BasicPageComponent},
-      {path: 'dynamic', component: BasicPageComponent},
-      {path: 'switches', component: BasicPageComponent},
+      {path: 'dynamic', component: DynamicPageComponent},
+      {path: 'switches', component: SwitchesPageComponent},
       {path: '**', component: BasicPageComponent}
     ]
   }
