9cc12a2367e9197b1300bf0699cf137907ee55fd
kenier <kenier202@gmail.com>
2024-11-19 16:09:46 -0500

    feat: add dynamic form with FormArray for favorite games

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: ``
 })
-export class DynamicPageComponent {
+export class DynamicPageComponent implements OnInit {
+
+  public myForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      favoriteGames: this.fb.array([
+        ['Metal Gear Solid', Validators.required],
+        ['Resident Evil', Validators.required],
+        ['The Last of Us', Validators.required],
+      ]),
+    });
+  }
+
+  get favoriteGames() {
+    return this.myForm.controls['favoriteGames'] as FormArray;
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 
 }
