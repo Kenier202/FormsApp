c265cde0322c2927fc42d04a31bab44aaa6d9455
kenier <kenier202@gmail.com>
2024-11-20 16:40:28 -0500

    feat(register-pages): linked form with formbuilder

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
 
+  public miForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.miForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required, Validators.minLength(6)],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  isValidField(field: string) {
+  }
+
+  onSubmit(): void {
+    this.miForm.markAllAsTouched();
+  }
 }
