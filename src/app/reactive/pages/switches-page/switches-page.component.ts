7b4d43adf00b32054960ae1787b366ff26677d26
kenier <kenier202@gmail.com>
2024-11-20 15:46:17 -0500

    feat(switches-pages): linked form with formbuilder

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit{
 
+  public myform!: FormGroup;
+
+  constructor (private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.myform = this.fb.group({
+      gender : ['M',Validators.required],
+      wantNotifications : [true,Validators.required],
+      termsAndConditions : [false,Validators.requiredTrue],
+
+    });
+  }
+
+  isValidField(field : string) : boolean | null {
+    return this.myform.controls[field].errors && this.myform.controls[field].touched;
+  }
+
+
+
+  onsave(): void {
+    if (this.myform.invalid) {
+      this.myform.markAllAsTouched();
+      return;
+    }
+  }
 }
