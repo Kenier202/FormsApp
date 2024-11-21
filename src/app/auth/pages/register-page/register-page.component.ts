2f2d31ffc3b80300accbfabdf0461f2fa2180819
kenier <kenier202@gmail.com>
2024-11-21 17:16:11 -0500

    feat(register-page): enhance username validation to prevent entry

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -12,11 +13,11 @@ export class RegisterPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.miForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      username: ['', Validators.required],
-      password: ['', Validators.required, Validators.minLength(6)],
-      confirmPassword: ['', Validators.required],
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, cantBeStrider]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
     });
   }
 
