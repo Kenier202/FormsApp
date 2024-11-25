bf26c793cc0bf7c729c57029fd700fa676bdd160
kenier <kenier202@gmail.com>
2024-11-25 10:19:37 -0500

    feat(register-page): integrate EmailValidatorService for enhanced email validation on registration form

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from '../../../shared/validators/validators';
 import { ValidatorsService } from '../../../shared/service/validators.service';
+import { EmailVlaidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -12,12 +13,14 @@ export class RegisterPageComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private validatorService : ValidatorsService) {}
+    private validatorService : ValidatorsService,
+    private emailVlaidatorService : EmailVlaidatorService
+  ) {}
 
   ngOnInit(): void {
     this.miForm = this.fb.group({
       name: ['', [Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
-      email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern) ]],
+      email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern)], [this.emailVlaidatorService]],
       username: ['', [Validators.required, this.validatorService.cantBeStrider]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', [Validators.required]],
