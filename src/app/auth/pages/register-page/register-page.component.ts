341aedc87fb4cb80cfbd43b4fea65d42264925f8
kenier <kenier202@gmail.com>
2024-11-25 09:58:35 -0500

    feat(register-page): implement dynamic error messages based on field validation status in registration form

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/service/validators.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -9,19 +10,22 @@ export class RegisterPageComponent implements OnInit {
 
   public miForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorService : ValidatorsService) {}
 
   ngOnInit(): void {
     this.miForm = this.fb.group({
-      name: ['', [Validators.required, Validators.pattern(firstNameAndLastnamePattern)]],
-      email: ['', [Validators.required, Validators.pattern(emailPattern) ]],
-      username: ['', [Validators.required, cantBeStrider]],
+      name: ['', [Validators.required, Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
+      email: ['', [Validators.required, Validators.pattern(this.validatorService.emailPattern) ]],
+      username: ['', [Validators.required, this.validatorService.cantBeStrider]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', [Validators.required]],
     });
   }
 
   isValidField(field: string) {
+    return this.validatorService.isValidField(this.miForm, field);
   }
 
   onSubmit(): void {
