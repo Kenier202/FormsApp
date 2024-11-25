5b8126b7ce696c07a0fcae3b6fcaa778a9a22784
kenier <kenier202@gmail.com>
2024-11-25 10:49:12 -0500

    feat(validators): enhance EmailValidatorService with async validation for email availability, returns error for exist email

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { Observable, of } from 'rxjs';
+import { observable, Observable, of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class EmailVlaidatorService implements AsyncValidator {
 
   validate(control: AbstractControl):Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log(email);
-    return of({emailTaken: true});
+
+    const httpCallObservable = new Observable<ValidationErrors | null>( ( subscriber ) => {
+      console.log({email});
+
+      if (email === 'strider@gmail.com') {
+        subscriber.next({emailTaken: true});
+        subscriber.complete();
+        return;
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+
+    });
+
+    return httpCallObservable;
   }
 
-  registerOnValidatorChange?(fn: () => void): void {
-    throw new Error('Method not implemented.');
-  }
+  // validate(control: AbstractControl):Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log(email);
+  //   return of({emailTaken: true});
+  // }
 
-
+  // registerOnValidatorChange?(fn: () => void): void {
+  //   throw new Error('Method not implemented.');
+  // }
 }
